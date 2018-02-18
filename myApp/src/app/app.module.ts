1802ba2e3ca0522b64884058dedef72c1697d173
Brettgraham13 <bgraham@macalester.edu>
2018-02-18 15:25:50 -0600

    Modifications to canvas page

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CanvasPage } from '../pages/canvas/canvas';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     ContactPage,
+    CanvasPage
     HomePage,
     TabsPage
   ],
