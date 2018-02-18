f97f1dd99bc5d82ce1534978818044aa24e92fc0
Ionatan Kuperwajs <ionatankuperwajs@Ionatans-MacBook-Pro.local>
2018-02-18 16:20:16 -0600

    Refactored some pages

--- myApp/src/pages/calc/calc.ts
+++ myApp/src/pages/calc/calc.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-calc',
+  templateUrl: 'calc.html'
 })
-export class HomePage {
+export class CalcPage {
 
   constructor(public navCtrl: NavController) {
 
