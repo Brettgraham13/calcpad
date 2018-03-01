8803593c6342d4d535c25d1a3ad96e6131b66a1c
Ionatan Kuperwajs <ionatankuperwajs@Ionatans-MacBook-Pro.local>
2018-03-01 13:17:59 -0700

    updated contact pages

--- myApp/src/pages/canvas/canvas.module.ts
+++ myApp/src/pages/canvas/canvas.module.ts
@@ -1,13 +1,13 @@
-// import { NgModule } from '@angular/core';
-// import { IonicPageModule } from 'ionic-angular';
-// import { CanvasPage } from './canvas';
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CanvasPage } from './canvas';
 
-// @NgModule({
-//   declarations: [
-//     CanvasPage,
-//   ],
-//   imports: [
-//     IonicPageModule.forChild(CanvasPage),
-//   ],
-// })
-// export class CanvasPageModule {}
+@NgModule({
+  declarations: [
+    CanvasPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CanvasPage),
+  ],
+})
+export class CanvasPageModule {}
